f561ec1758b8ecf5900413908a139da83694a0a8
Rifal21 <giantjou77@gmail.com>
2026-02-08 12:21:48 +0700

    fix: url required

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Paginator::useTailwind();
+        if (str_contains(config('app.url'), 'https://') || $this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
